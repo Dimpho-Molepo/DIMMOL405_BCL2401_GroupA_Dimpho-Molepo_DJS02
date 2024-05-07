04872410e0d41dfb3be790861c586ff78a9a84fc
Dimpho Molepo <136012291+Dimpho-Molepo@users.noreply.github.com>
2024-05-07 16:42:15 +0200

    Refacted by combining the if statements

--- Whole Number Divider/scripts.js
+++ Whole Number Divider/scripts.js
@@ -6,25 +6,21 @@ form.addEventListener("submit", (event) => {
   const entries = new FormData(event.target);
   const { dividend, divider } = Object.fromEntries(entries);
 
-  result.innerText = Math.floor(dividend / divider);
-
   if (divider === "0" ) {
     result.classList.add("error-message");
     result.innerText = "Division not performed. Invalid number provided. Try again";
-    console.error(new Error("Invalid reuslt, divider can not be zero"));
-  }
-
-  if (dividend === '' && divider === ''){
+    console.error(new Error("Invalid result, divider can not be zero"));
+  } else if (dividend === '' && divider === ''){
     result.classList.add("error-message");
     result.innerText = "Division not performed. Both values are required in inputs. Try again.";
     console.error(new Error("Both values are required in inputs."))
-  }
-
-  if (dividend.match(/[^0-9]/) || divider.match(/[^0-9]/)) {
+  } else if (dividend.match(/[^0-9]/) || divider.match(/[^0-9]/)) {
     const criticalError = document.createElement("div");
     criticalError.classList.add("critical-error");
     criticalError.textContent = "Something critical went wrong. Please reload the page"
     document.body.append(criticalError);
     console.error(new Error("Invalid number"));
+  } else {
+    result.innerText = Math.floor(dividend / divider);
   }
 });
